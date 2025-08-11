04b6b4d1e39bf03afa345329adf488394602728a
proudhAteR <158060444+proudhAteR@users.noreply.github.com>
2025-08-10 23:11:45 -0400

    fix: The app configuration is not found when trying to launch it so I tried a better approach on the file loading

--- Infrastructure/Services/Files/_internal/Secrets.py
+++ Infrastructure/Services/Files/_internal/Secrets.py
@@ -1,19 +1,24 @@
+import importlib.resources as pkg_resources
 import json
 import os
 
+import Conf
 from Infrastructure.Services.Files._internal.ProjectPaths import ProjectPaths
 
 
 class Secrets:
     @staticmethod
-    def load_config(name: str = "GOOGLE_CREDENTIALS") -> dict:
+    def load_config(name: str) -> dict:
         if not os.getenv(name):
-            cred_path = ProjectPaths.root() / "Conf" / ".google_credentials.json"
+            cred_filename = f".{name.lower()}.json"
+            cred_path = ProjectPaths.root() / "Conf" / cred_filename
+
             if cred_path.exists():
                 with open(cred_path, "r", encoding="utf-8") as f:
                     os.environ[name] = f.read()
             else:
-                raise FileNotFoundError(f"Google credentials file not found at {cred_path}")
+                with pkg_resources.files(Conf).joinpath(cred_filename).open("r", encoding="utf-8") as f:
+                    os.environ[name] = f.read()
 
         raw = os.getenv(name)
         return json.loads(raw) if raw else {}
